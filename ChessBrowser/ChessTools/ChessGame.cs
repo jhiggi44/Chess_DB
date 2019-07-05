da2c268952bd89778752a727dcd180828a27d74f
jhiggi44 <jordandhiggins@gmail.com>
2019-07-04 17:49:52 -0700

    working on populating tables

---
+++ ChessBrowser/ChessTools/ChessGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTools
+{
+    public class ChessGame {
+        public string Event, Site, EventDate, WhitePlayer, BlackPlayer, Result, Moves;
+        public int WhiteElo, BlackElo;
+        public ChessGame(string ev, string site, string date, string wPlayer, string bPlayer, int wElo, int bElo, string res, string moves) {
+            Event = ev;
+            Site = site;
+            EventDate = date;
+            WhitePlayer = wPlayer;
+            BlackPlayer = bPlayer;
+            WhiteElo = wElo;
+            BlackElo = bElo;
+            Result = res;
+            Moves = moves;
+        }
+
+        public void print() {
+            Console.WriteLine("Event: " + Event + "\nSite: " + Site + "\nEventDate: " + EventDate);
+            Console.WriteLine("WhitePlayer: " + WhitePlayer + "\nBlackPlayer: " + BlackPlayer + "\nWinner: " + Result);
+            Console.WriteLine("WhiteElo: " + WhiteElo + "\nBlackElo: " + BlackElo + "\nMoves: " + Moves + "\n\n");
+        }
+    }
+}
---
+++ ChessBrowser/ChessTools/PGNReader.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ChessTools {
+    public class PGNReader {
+        List<string> lines = new List<string>();
+
+        public PGNReader(string fileName) {
+            string[] temp = File.ReadAllLines(fileName);
+            foreach (string t in temp) {
+                string line = t;
+                if (line.Length > 0 && line[0] == '[') {
+                    line = t.Remove(0, 1);
+                }
+                lines.Add(line);
+            }
+        }
+        
+        public List<ChessGame> extractGameData() {
+            List<ChessGame> games = new List<ChessGame>();
+            string ev, site, date, wPlayer, bPlayer, res, moves;
+            int wElo, bElo;
+            int i = 0;
+            while (i < lines.Count) {
+                ev = site = date = wPlayer = bPlayer = res = moves = "";
+                bElo = wElo = 0;
+                while (!string.IsNullOrWhiteSpace(lines[i])) {
+                    if (lines[i].StartsWith("Event ")) {
+                        ev = extractSubStr(lines[i]);
+                    } else if (lines[i].StartsWith("Site ")) {
+                        site = extractSubStr(lines[i]);
+                    } else if (lines[i].StartsWith("White ")) {
+                        wPlayer = extractSubStr(lines[i]);
+                    } else if (lines[i].StartsWith("Black ")) {
+                        bPlayer = extractSubStr(lines[i]);
+                    } else if (lines[i].StartsWith("Result ")) {
+                        string resStr = extractSubStr(lines[i]);
+                        if (resStr.Contains("0-1")) {
+                            res = "B";
+                        } else if (resStr.Contains("1-0")) {
+                            res = "W";
+                        } else {
+                            res = "D";
+                        }
+                    } else if (lines[i].StartsWith("WhiteElo ")) {
+                        string eloStr = extractSubStr(lines[i]);
+                        wElo = Convert.ToInt32(eloStr);
+                    } else if (lines[i].StartsWith("BlackElo ")) {
+                        string eloStr = extractSubStr(lines[i]);
+                        bElo = Convert.ToInt32(eloStr);
+                    } else if (lines[i].StartsWith("EventDate ")) {
+                        date = extractSubStr(lines[i]);
+                    }
+                    i++;
+                }
+
+                i++;
+
+                while (!string.IsNullOrWhiteSpace(lines[i])) {
+                    moves += lines[i];
+                    i++;
+                }
+                i++;
+                games.Add(new ChessGame(ev, site, date, wPlayer, bPlayer, wElo, bElo, res, moves));
+            }
+            return games;
+        }
+
+        private string extractSubStr(string line) {
+            int start = line.IndexOf('"') + 1;
+            return line.Substring(start, (line.Length - 2) - start);
+        }
+
+    }
+}
